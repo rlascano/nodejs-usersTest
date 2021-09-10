de5933848f103fc32161d793e7c3afdacfd8f20c
rlascano <rodrigo.lascano@gmail.com>
2021-09-09 21:17:10 -0300

    Moved hash logic to User model

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const bcrypt = require('bcrypt')
 const User = require('../models/user')
 
 router.get('/', async (req, res) => {
@@ -13,12 +12,10 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-    try{
-        const salt = await bcrypt.genSalt()
-        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+    try{        
         const user = new User({
             name: req.body.name,
-            password: hashedPassword
+            password: req.body.password
         })        
         const newUser = await user.save()
         res.status(201).send()
@@ -33,13 +30,12 @@ router.post('/login', async (req, res) => {
         const user = await User.findOne({name: req.body.name})
         if(user == null) {
             return res.status(400).send("Cannot find user")
+        }  
+        const valid = await user.isValidPassword(req.body.password)           
+        if(!valid) {            
+            return res.status(400).send("Invalid name or password")
         }
-        if(await bcrypt.compare(req.body.password, user.password)){
-            res.send("Success")            
-        }
-        else {
-            res.send("Not Allowed")
-        }
+        res.status(201).send("Success")
     } catch(err) {
         res.status(500).json({err: err.message})
     }
